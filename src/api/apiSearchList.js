0941035a724a46657f87d2da8c5499b67abd0739
Oliver Tuck <olivertuckwebdev@gmail.com>
2019-05-07 20:43:12 +0100

    Fixed a bug where the selected channel wasn't updating when changing the selected result

--- src/api/apiSearchList.js
+++ src/api/apiSearchList.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 15,
+    maxResults: 10,
     type: 'video',
     key: process.env.REACT_APP_API_KEY
   }
--- src/components/App.js
+++ src/components/App.js
@@ -12,50 +12,79 @@ class App extends Component {
   state = {
     searchTerm: '',
     searchResults: [],
+    searchResultsChannels: [],
     selectedResult: null,
     selectedResultChannel: null
   };
 
   onSearchSubmit = async searchTerm => {
+    // Search for videos matching the search term
     const searchListResponse = await apiSearchList.get('/search', {
       params: {
         q: searchTerm
       }
     });
 
+    // Get the id of each search result in an array then join them into a comma
+    // separated list
     const searchListIds = searchListResponse.data.items
       .map(({ id }) => id.videoId)
       .join();
 
+    // Get the videos matching the ids from the search results
     const videoListResponse = await apiVideoList.get('/videos', {
       params: {
         id: searchListIds
       }
     });
 
+    // Get the channel id from each search result in an array then join them into a comma
+    // separated list
+    const searchListChannelIds = searchListResponse.data.items
+      .map(({ snippet }) => snippet.channelId)
+      .join();
+
+    // Get the channels matching the ids
     const channelListResponse = await apiChannelList.get('/channels', {
       params: {
-        id: videoListResponse.data.items[0].snippet.channelId
+        id: searchListChannelIds
       }
     });
 
+    // Get the channel pertaining to first search result
+    const selectedResultChannel = channelListResponse.data.items.filter(
+      channel =>
+        channel.id === videoListResponse.data.items[0].snippet.channelId
+    );
+
+    // Update the application state to contain the search results, search term,
+    // selected result (first result in this case), and the channel of the selected result
     this.setState({
       searchResults: videoListResponse.data.items,
+      searchResultsChannels: channelListResponse.data.items,
       searchTerm,
       selectedResult: videoListResponse.data.items[0],
-      selectedResultChannel: channelListResponse.data.items[0]
+      selectedResultChannel: selectedResultChannel[0]
     });
 
     navigate(`/video/${this.state.selectedResult.id}`);
   };
 
   updateSelectedResult = id => {
+    // Get the id of the clicked result and retrieve the search results pertaining to that id
     const selectedResult = this.state.searchResults.filter(
       result => result.id === id
     );
 
+    // Get the id of the channel of the new selected result and retreive the channel pertaining
+    // to that result
+    const selectedResultChannel = this.state.searchResultsChannels.filter(
+      channel => channel.id === selectedResult[0].snippet.channelId
+    );
+
     this.setState({
-      selectedResult: selectedResult[0]
+      selectedResult: selectedResult[0],
+      selectedResultChannel: selectedResultChannel[0]
     });
   };
 
